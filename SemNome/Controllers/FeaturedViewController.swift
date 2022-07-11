82e779d5d77be275ea6732478f62c13f733112ac
João Pedro <jpcm2@cin.ufpe.br>
2022-07-11 15:04:50 -0300

    Criando as classes para as collection view

---
+++ SemNome/Controllers/FeaturedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeaturedViewController.swift
+//  SemNome
+//
+//  Created by jpcm2 on 05/07/22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController {
+
+    @IBOutlet var popularCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    @IBOutlet var upComingCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ SemNome/View/PopularCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PopularCollectionViewCell.swift
+//  SemNome
+//
+//  Created by jpcm2 on 11/07/22.
+//
+
+import UIKit
+
+class PopularCollectionViewCell: UICollectionViewCell {
+    
+}
